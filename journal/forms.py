55ddffc6baad3ecfe071c9e5665003d39a49185f
nirbo <nirbo2@gmail.com>
2017-04-10 01:27:31 +0300

    Modified the Add_Server ModelForm to look much nicer.

--- journal/forms.py
+++ journal/forms.py
@@ -4,14 +4,20 @@ from journal.models import Server, Location
 
 
 class AddServerForm(forms.ModelForm):
-    server_name = forms.CharField(label='Server Name')
-    server_mgmt_ip = forms.GenericIPAddressField(label='MGMT IP', protocol='ipv4')
-    server_data1_ip = forms.GenericIPAddressField(label='Data Network 1', protocol='ipv4')
-    server_data2_ip = forms.GenericIPAddressField(label='Data Network 2', protocol='ipv4')
-    server_owner = forms.CharField(label='Server Owner')
+    server_name = forms.CharField(label='Server Name',
+                                  widget=forms.TextInput(attrs={'class': "form-control"}))
+    server_mgmt_ip = forms.GenericIPAddressField(label='MGMT IP', protocol='ipv4',
+                                                 widget=forms.TextInput(attrs={'class': "form-control"}))
+    server_data1_ip = forms.GenericIPAddressField(label='Data Network 1', protocol='ipv4',
+                                                  widget=forms.TextInput(attrs={'class': "form-control"}))
+    server_data2_ip = forms.GenericIPAddressField(label='Data Network 2', protocol='ipv4',
+                                                  widget=forms.TextInput(attrs={'class': "form-control"}))
+    server_owner = forms.CharField(label='Server Owner',
+                                   widget=forms.TextInput(attrs={'class': "form-control"}))
     server_location = forms.ModelChoiceField(label='Server Location',
                                              queryset=Location.objects.all(),
-                                             empty_label='Select Server Location')
+                                             empty_label='Select Server Location',
+                                             widget=forms.Select(attrs={'class': "form-control"}))
 
     class Meta:
         model = Server
