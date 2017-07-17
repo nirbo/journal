3d9a6eb4bea1735e81330765e71415073160acad
Nir Ben-Or <nirbo2@gmail.com>
2017-07-17 16:01:49 +0300

    Registered the 'VirtualIP' model to Django's admin.

--- journal/admin.py
+++ journal/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from journal.models import Server, Location, Owner
+from journal.models import Server, Location, Owner, VirtualIP
 
 # Register your models here.
 admin.site.register(Server)
 admin.site.register(Location)
 admin.site.register(Owner)
+admin.site.register(VirtualIP)
